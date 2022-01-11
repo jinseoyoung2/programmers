22a0ef3e967e18050a81e8bf73539cf4b9db6130
Jin seo young <44155993+jinseoyoung2@users.noreply.github.com>
2022-01-11 17:33:42 +0900

    Add files via upload

---
+++ java/음양 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int res = 0;
+        for(int i=0; i<signs.length; i++){
+            if(signs[i]){
+                res+=absolutes[i];
+            }else{
+                res-=absolutes[i];
+            }
+        }
+        return res;
+    }
+}
