ab59eb10661ea31487ecc797516d90fdd8705cf1
Jin seo young <44155993+jinseoyoung2@users.noreply.github.com>
2022-01-27 12:24:22 +0900

    Add files via upload

---
+++ java/행렬의 덧셈.java
@@ -0,0 +1,12 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12950?language=java
+
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        for(int i=0; i<arr1.length; i++){
+            for(int j=0; j<arr1[0].length; j++){
+                arr1[i][j] += arr2[i][j];
+            }
+        }
+        return arr1;
+    }
+}
