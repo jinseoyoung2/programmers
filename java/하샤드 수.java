5e9e9cd450e1dca55c824305d3292ada7982f99f
Jin seo young <44155993+jinseoyoung2@users.noreply.github.com>
2022-01-28 11:02:10 +0900

    Add files via upload

---
+++ java/하샤드 수.java
@@ -0,0 +1,16 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12947
+
+class Solution {
+    public boolean solution(int x) {
+        String[] y = String.valueOf(x).split("");
+        int z = 0;
+        for(int i=0;i<y.length;i++){
+            z+=Integer.parseInt(y[i]);
+        }
+        if(x%z==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
