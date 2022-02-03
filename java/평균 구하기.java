7b62c6feec2be4bedd9b54ee098d2e206a0feb6d
Jin seo young <44155993+jinseoyoung2@users.noreply.github.com>
2022-02-04 04:55:31 +0900

    Add files via upload

---
+++ java/평균 구하기.java
@@ -0,0 +1,11 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12944?language=java
+
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        for(int i=0; i<arr.length; i++){
+            answer+=arr[i];
+        }
+        return answer/(double)arr.length;
+    }
+}
